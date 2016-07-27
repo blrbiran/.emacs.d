6f0287cbca15724b7ca7ce842f1f7c4b7696654e
blrbiran <blrbiran@163.com>
2016-07-28 03:50:36 +0800

    change org-latex related settings.

--- lisp/init-my-org-latex.el
+++ lisp/init-my-org-latex.el
@@ -1,11 +1,18 @@
+;; see http://joat-programmer.blogspot.de/2013/07/org-mode-version-8-and-pdf-export-with.html
 ;;special settings for Org-Latex
+;; Include the latex-exporter
 (require 'ox-latex)
+;; Add minted to the defaults packages to include when exporting.
 (add-to-list 'org-latex-packages-alist '("" "minted"))
+;; Tell the latex export to use the minted package for source
+;; code coloration.
 (setq org-latex-listings 'minted)
-
+;; Let the exporter use the -shell-escape option to let latex
+;; execute external programs.
+;; This obviously and can be dangerous to activate!
 (setq org-latex-pdf-process
-      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+      '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+;; (setq org-latex-pdf-process
+;;       '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
 (provide 'init-my-org-latex)
